627bb314a3feb8838599033da66bb81583ea9a91
Yulin Li <148395276+Michelleyukli@users.noreply.github.com>
2024-04-09 22:09:25 +0000

    update changes

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
 import pytz
+import sqlite3
+con = sqlite3.connect("week3.sqlite", isolation_level=None)
+cur = con.cursor()
